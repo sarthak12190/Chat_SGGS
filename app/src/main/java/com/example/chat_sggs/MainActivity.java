4086e927c48a2e8e5231782a9d0a5027a50a5275
Dhanshri Gatthawar <2020bcs106@sggs.ac.in>
2022-07-11 13:52:28 +0530

    Done it

--- app/src/main/java/com/example/chat_sggs/MainActivity.java
+++ app/src/main/java/com/example/chat_sggs/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 //hello shreyaNerkar
-//Done it
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
