76ab050d6f6a760bc70179283c14aeec164ae502
Sarthak kulkarni <kulkarnisarthak0112@gmail.com>
2022-07-11 11:48:22 +0530

    Chat SGGS

---
+++ app/src/main/java/com/example/chat_sggs/Signup_activity.java
@@ -0,0 +1,56 @@
+package com.example.chat_sggs;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.chat_sggs.databinding.ActivitySignupBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class Signup_activity extends AppCompatActivity {
+
+    ActivitySignupBinding binding;
+    private FirebaseAuth auth;
+    FirebaseDatabase database;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignupBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getSupportActionBar().hide();
+
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
+        binding.clickSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                auth.createUserWithEmailAndPassword
+                        (binding.etEmail.getText().toString(), binding.etPassword.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(Signup_activity.this, "User created Succesfully", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(Signup_activity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+            }
+        });
+
+    }
+}
